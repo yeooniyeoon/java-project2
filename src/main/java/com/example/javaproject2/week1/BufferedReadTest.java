0319db7585a0da148421a9b69201742bc3caae5b
ycyeon <ickite428@gmail.com>
2023-04-19 14:46:00 +0900

    feat: BufferedReaderEx, BufferedReadTest 추가 - readALine() 한문장 읽기

---
+++ src/main/java/com/example/javaproject2/week1/BufferedReadTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+import java.io.IOException;
+
+public class BufferedReadTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReaderEx bufferedReaderEx = new BufferedReaderEx();
+        bufferedReaderEx.readALine();
+    }
+}
