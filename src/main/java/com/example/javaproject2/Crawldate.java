2965d4b80d539ca0de1151920ab9e8dbfbeecfd2
ycyeon <ickite428@gmail.com>
2023-04-19 13:10:08 +0900

    feat: 마지막 크롤 시간 작업1 - CrawlDate 도메인 추가

---
+++ src/main/java/com/example/javaproject2/Crawldate.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2;
+
+import java.time.LocalDateTime;
+
+public class Crawldate {
+    private LocalDateTime lastCrawlDateTeime;
+
+    // alt + insert
+    public Crawldate(LocalDateTime lastCrawlDateTeime) {
+        this.lastCrawlDateTeime = lastCrawlDateTeime;
+    }
+
+
+}
