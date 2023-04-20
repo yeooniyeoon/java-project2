6fedbf2ee03fe5bbb60ed6420e87ddb5c540daf8
ycyeon <ickite428@gmail.com>
2023-04-20 15:52:21 +0900

    feat: Codeup1015 추가. - 실수 입력받아 둘째 자리까지 출력.

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1015.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float fVal = sc.nextFloat();
+        System.out.printf("%.2f", fVal);
+    }
+}
