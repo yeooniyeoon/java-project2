3fe78a16c832da2b032490bb962caf6e3dd8366f
ycyeon <ickite428@gmail.com>
2023-04-20 10:45:36 +0900

    feat: ScannerEx2, ScannerEx2Test, Codeup1010 추가 - 안내문 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1010.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerEx2Test.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1.day4;
+
+public class ScannerEx2Test {
+    public static void main(String[] args) {
+        ScannerEx2 scannerEx2 = new ScannerEx2();
+        scannerEx2.inputTwoNumbersAndPlus();
+    }
+}
