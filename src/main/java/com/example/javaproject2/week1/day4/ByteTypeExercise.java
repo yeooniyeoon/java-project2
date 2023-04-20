254b2c340651f6498295fb4b1329143697d51693
ycyeon <ickite428@gmail.com>
2023-04-20 15:17:33 +0900

    feat: ByteTypeExercise, ScannerFormatting, VariableEx 추가. - ByteTypeExercise -> byte 타입에 담을 수 있는 값의 범위. - ScannerFormatting -> 값을 입력받아 변수에 저장한 뒤 출력 - VariableEx -> 변수 선언 및 초기화

---
+++ src/main/java/com/example/javaproject2/week1/day4/ByteTypeExercise.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1.day4;
+
+public class ByteTypeExercise {
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = -127;
+        byte byte3 = -128;
+        //byte byte4 = -129;
+        //byte byte5 = 128;
+        // 1byte는 8bit이므로 -128~127까지 256개의 담을 수 있음
+        System.out.println(byte1);
+    }
+}
+
