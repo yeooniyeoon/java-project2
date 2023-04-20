13bbc9f443d363eb32b875df8ae8b981c56c2d52
ycyeon <ickite428@gmail.com>
2023-04-20 15:41:13 +0900

    feat: Codeup1014 추가. - 두 문자 입력받아 순서를 바꿔 출력

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1014.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String val1 = sc.next();
+        String val2 = sc.next();
+        System.out.printf("%s %s", val2, val1);
+    }
+}
