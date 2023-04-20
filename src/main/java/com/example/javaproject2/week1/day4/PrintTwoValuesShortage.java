eb865e805be74f8a25791ca725e3f250a2724d63
ycyeon <ickite428@gmail.com>
2023-04-20 11:29:04 +0900

    feat: PrintfEx, PrintTwoValuesShortage, MultiplicationTable 추가 - PrintfEx -> printf 실습. PrintTwoValuesShortage -> printf에서 값이 부족한 경우. MultiplicationTable -> 구구단

---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%님 안녕하세요. 제가 하고 싶은 말은 %s 입니다", "유채연");
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/day4/PrintfEx.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day4;
+
+public class PrintfEx {
+    public void print() {
+        System.out.printf("%s", "Hello");
+    }
+
+    public static void main(String[] args) {
+        PrintfEx printfEx = new PrintfEx();
+        printfEx.print();
+    }
+}
