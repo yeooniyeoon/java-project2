dcbf6c7f8eb6bb9f5044302e195e42e88dbbf289
ycyeon <ickite428@gmail.com>
2023-04-20 15:32:31 +0900

    feat: DefineAndInitialize, ScannerAndVariable 추가. - DefineAndInitialize -> 변수 선언 및 초기화 - ScannerAndVariable -> 변수에 값 한개씩 담아서 더하기

---
+++ src/main/java/com/example/javaproject2/week1/day4/DefineAndInitialize.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.javaproject2.PrintHello;
+
+import java.util.Scanner;
+
+public class DefineAndInitialize {
+    public static void main(String[] args) {
+        int iVal = 1;
+        PrintHello printHello = new PrintHello();
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
