dff53b1b0a596258389da405adaeef15586176b1
ycyeon <ickite428@gmail.com>
2023-04-20 09:40:41 +0900

    feat: ScannerStrEx, ScannerStrExTest 추가 - 문자열 두개 입력받은 후 합쳐서 출력

---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    void inputStringAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() + sc.next());
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/day4/ScannerStrExTest.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1.day4;
+
+import com.example.javaproject2.week1.ScannerEx;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx scannerStrEx = new ScannerStrEx();
+        scannerStrEx.inputStringAndPlus();
+    }
+}
