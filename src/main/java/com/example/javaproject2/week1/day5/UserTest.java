d6f300242c0c83a9cc8b0c23ac07f5601b3991f3
ycyeon <ickite428@gmail.com>
2023-04-21 15:17:37 +0900

    feat: User, UserTest 추가 - User Object 생성

---
+++ src/main/java/com/example/javaproject2/week1/day5/UserTest.java
@@ -0,0 +1,15 @@
+package com.example.javaproject2.week1.day5;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User yeoon = new User();
+        yeoon.name = "유채연";
+        yeoon.phoneNumber = "010-1234-5678";
+        yeoon.age = 24;
+
+        User jaesung = new User();
+        jaesung.name = "재성 매니저님";
+        jaesung.phoneNumber = "010-1234-5678";
+        jaesung.age = 24;
+    }
+}
