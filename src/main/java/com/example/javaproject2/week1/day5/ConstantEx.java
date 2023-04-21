4a2e6d9717973097d30decf4c02b999c0264cc5e
ycyeon <ickite428@gmail.com>
2023-04-21 11:46:28 +0900

    feat: ConstantEx, MyAccountEx, SpaceInvader 추가. - 멤버변수, 지역변수 실습

---
+++ src/main/java/com/example/javaproject2/week1/day5/ConstantEx.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week1.day5;
+
+import com.example.javaproject2.PrintHello;
+
+public class ConstantEx {
+    public final int MAX_LEVEL = 3;
+    public static final int MIN_LEVEL = 1;
+    public static void main(String[] args) {
+        final int iVal;
+        iVal = 1;
+        System.out.println(iVal);
+
+        final PrintHello printHello = new PrintHello();
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week1/day5/SpaceInvader.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week1.day5;
+
+public class SpaceInvader {
+    int location = 0;
+    private void moveLeft() {
+        location -= 1;
+    }
+
+    private void moveRight() {
+        location += 1;
+    }
+}
