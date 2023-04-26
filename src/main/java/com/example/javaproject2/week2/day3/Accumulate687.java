c364f38df9940821b43e3db110299602dfd71276
ycyeon <ickite428@gmail.com>
2023-04-26 16:09:39 +0900

    feat: Accumulate687, StringAccu 추가 - StringAccu : 문자열 누적하기 - Accumulate687 : 나눈 결과 누적하여 더하기

---
+++ src/main/java/com/example/javaproject2/week2/day3/Accumulate687.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.week2.day3;
+
+public class Accumulate687 {
+    public static void main(String[] args) {
+        int num = 687;
+        int answer = 0;
+        answer = answer + num % 10;
+        System.out.printf("answer :%d\n", answer);
+        num = num / 10;
+        answer = answer + num % 10;
+        System.out.printf("answer :%d\n", answer);
+        num = num / 10;
+        answer = answer + num % 10;
+        System.out.printf("answer :%d\n", answer);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/day3/StringAccu.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day3;
+
+public class StringAccu {
+    public static void main(String[] args) {
+        String answer = "";
+        answer = answer + "쿵";
+        System.out.println(answer);
+        answer = answer + "짝";
+        System.out.println(answer);
+
+
+    }
+}
