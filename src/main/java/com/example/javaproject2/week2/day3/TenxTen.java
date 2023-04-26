d727461e9d5a62eb0e95acb8833fdd530bf01252
ycyeon <ickite428@gmail.com>
2023-04-26 10:37:03 +0900

    feat: TenxTen, ThreexThree 추가

---
+++ src/main/java/com/example/javaproject2/week2/day3/TenxTen.java
@@ -0,0 +1,21 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class TenxTen {
+    public static void main(String[] args) {
+        int[][] arr10x10 = new int[10][10];
+        System.out.println(Arrays.toString(arr10x10[0]));
+        System.out.println(Arrays.toString(arr10x10[1]));
+        System.out.println(Arrays.toString(arr10x10[2]));
+        System.out.println(Arrays.toString(arr10x10[3]));
+        System.out.println(Arrays.toString(arr10x10[4]));
+        System.out.println(Arrays.toString(arr10x10[5]));
+        System.out.println(Arrays.toString(arr10x10[6]));
+        System.out.println(Arrays.toString(arr10x10[7]));
+        System.out.println(Arrays.toString(arr10x10[8]));
+        System.out.println(Arrays.toString(arr10x10[9]));
+
+
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week2/day3/ThreexThree.java
@@ -0,0 +1,12 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class ThreexThree {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        System.out.println(Arrays.toString(arr[0]));
+        System.out.println(Arrays.toString(arr[1]));
+        System.out.println(Arrays.toString(arr[2]));
+    }
+}
