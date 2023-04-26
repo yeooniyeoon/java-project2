c3a37e2794439a9ec31ff3514e6a405fcd8085e2
ycyeon <ickite428@gmail.com>
2023-04-26 10:40:58 +0900

    feat: TenxTenFor 추가 - for문 사용하여 2차원 배열 출력

---
+++ src/main/java/com/example/javaproject2/week2/day3/TenxTenFor.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class TenxTenFor {
+    public static void main(String[] args) {
+        int[][] arr10x10 = new int[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Arrays.toString(arr10x10[i]));
+        }
+    }
+}
