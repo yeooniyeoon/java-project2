814a93d8220cc4273c2a2249afb7b2464ee0ea9c
ycyeon <ickite428@gmail.com>
2023-04-27 09:30:01 +0900

    feat: ArraySum 추가 - 배열에 있는 값 누적하여 더하기

--- src/main/java/com/example/javaproject2/week2/day3/Accumulate687.java
+++ src/main/java/com/example/javaproject2/week2/day3/Accumulate687.java
@@ -3,14 +3,14 @@ package com.example.javaproject2.week2.day3;
 public class Accumulate687 {
     public static void main(String[] args) {
         int num = 687;
-        int answer = 0;
-        answer = answer + num % 10;
+        int answer = 0; // 누적할 변수 선언
+        answer = answer + num % 10; // 나머지 구해 누적하기
         System.out.printf("answer :%d\n", answer);
         num = num / 10;
-        answer = answer + num % 10;
+        answer = answer + num % 10; // 나머지 구해 누적하기
         System.out.printf("answer :%d\n", answer);
         num = num / 10;
-        answer = answer + num % 10;
+        answer = answer + num % 10;  // 나머지 구해 누적하기
         System.out.printf("answer :%d\n", answer);
     }
 }
