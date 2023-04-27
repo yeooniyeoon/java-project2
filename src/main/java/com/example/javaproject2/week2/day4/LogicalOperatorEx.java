98bd13b34a114cfdc75ccc92bf0e5fedbf53e5ad
ycyeon <ickite428@gmail.com>
2023-04-27 11:45:30 +0900

    feat: LogicalOperatorEx, BoilerControl 추가 - 논리 연산자 && 실습

---
+++ src/main/java/com/example/javaproject2/week2/day4/LogicalOperatorEx.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.week2.day4;
+
+public class LogicalOperatorEx {
+    public static void main(String[] args) {
+        int age = 25;
+        boolean isStudent = false;
+
+        if (age >= 18 && !isStudent) {
+            System.out.println("성인이며 학생이 아닙니다.");
+        }
+
+        boolean result = true && true;
+        System.out.printf("%b\n", result);
+        result = true && false;
+        System.out.printf("%b\n", result);
+        result = false && false;
+        System.out.printf("%b\n", result);
+    }
+}
