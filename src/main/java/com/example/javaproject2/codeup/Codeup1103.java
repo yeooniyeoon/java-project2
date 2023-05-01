356df1c43daf5c2d81ef6a7849eb6a863af92026
ycyeon <ickite428@gmail.com>
2023-05-01 09:15:50 +0900

    feat: Codeup1103,1106 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1103.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.codeup;
+
+public class Codeup1103 {
+    public static void main(String[] args) {
+        System.out.println("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1106.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.codeup;
+
+public class Codeup1106 {
+    public static void main(String[] args) {
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        System.out.printf("%d %d\n", min, max);
+    }
+}
