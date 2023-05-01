511fd1e5dae38c33017d967c9563c9823fa6a1d3
ycyeon <ickite428@gmail.com>
2023-05-01 14:49:12 +0900

    fix: MultiplicationTable 추가 - 특정 단 선택해서 출력하는 메소드 추가

--- src/main/java/com/example/javaproject2/week3/day1/MultiplicationTable.java
+++ src/main/java/com/example/javaproject2/week3/day1/MultiplicationTable.java
@@ -1,11 +1,16 @@
 package com.example.javaproject2.week3.day1;
 
 public class MultiplicationTable {
+    public static void printDan(int dan) {
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d\n", dan, i, dan * i);
+        }
+        System.out.println("==========");
+    }
     public static void main(String[] args) {
-        for (int i = 2; i <= 4; i++) {
-            for (int j = 1; j <= 9; j++) {
-                System.out.printf("%d * %d = %d\n", i, j, j * i);
-            }
+        int[] dan = {1, 3, 5, 7};
+        for (int i = 0; i <dan.length; i++) {
+            printDan(dan[i]);
         }
     }
 }
