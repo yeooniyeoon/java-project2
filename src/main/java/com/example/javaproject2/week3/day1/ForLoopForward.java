153772fb13ce729613bce3b7cea751106ef68d43
ycyeon <ickite428@gmail.com>
2023-05-01 10:10:31 +0900

    feat: ForLoopForward 추가 - for문 사용해서 0~9까지 출력

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForLoopForward.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForLoopForward {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
