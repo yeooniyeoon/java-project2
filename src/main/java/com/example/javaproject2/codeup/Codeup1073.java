7e23e5547d823c61055daf7c8dda9186847a7752
ycyeon <ickite428@gmail.com>
2023-05-01 11:44:09 +0900

    feat: Codeup1073~1075 추가

---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1073.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inputNum = 0;
+        for (int i = 0; true; i++) {
+            inputNum = sc.nextInt();
+            if (inputNum == 0)
+                break;
+            System.out.println(inputNum);
+        }
+    }
+}
---
+++ src/main/java/com/example/javaproject2/codeup/Codeup1074.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.codeup;
+
+import java.util.Scanner;
+
+public class Codeup1074 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inputNum = sc.nextInt();
+        for (int i = inputNum; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
