b50514c7e1b9092fcd56fcf5a7f1de40e31ad979
ycyeon <ickite428@gmail.com>
2023-05-01 10:16:40 +0900

    feat: ForLoopPrintHorizontal 추가 - for문 사용해서 0~9까지 개행없이 가로로 출력

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForLoopPrintHorizontal.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForLoopPrintHorizontal {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("%d ", i);
+        }
+    }
+}
