d206be1c1c1554306a90c4de5d937644f7138f2b
ycyeon <ickite428@gmail.com>
2023-05-01 13:26:36 +0900

    feat: ForLoopForwardStep, LoopChar 추가 - ForLoopForwardStep : 증감값을 2로 주어 10 미만의 홀수, 짝수 출력 - LoopChar : 알파벳 반복.

---
+++ src/main/java/com/example/javaproject2/week3/day1/LoopChar.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3.day1;
+
+public class LoopChar {
+    public static void main(String[] args) {
+        for (char c = 'a'; c <= 'z'; c++) {
+            //System.out.println(c);
+            System.out.printf("%c ", c);
+        }
+    }
+}
