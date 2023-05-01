ce6eec152662bfb7bc7fab94e1d6a1efd199a5bb
ycyeon <ickite428@gmail.com>
2023-05-01 11:28:42 +0900

    feat: ForReverse 추가 - 3가지 케이스 실습. (개행 포함 여부, 10~1, 10~0)

---
+++ src/main/java/com/example/javaproject2/week3/day1/ForReverse.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3.day1;
+
+public class ForReverse {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0 ; i--) {
+            // System.out.println(i);
+            System.out.printf("%d ", i);
+        }
+    }
+}
