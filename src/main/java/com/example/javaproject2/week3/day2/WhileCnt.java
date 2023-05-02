e145619f6c0daf4afd40d88f6078dfa5ba3fed0e
ycyeon <ickite428@gmail.com>
2023-05-02 14:06:03 +0900

    feat: WhileSleep, WhileCnt, WhileCntHori 추가

---
+++ src/main/java/com/example/javaproject2/week3/day2/WhileCnt.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week3.day2;
+
+public class WhileCnt {
+    public static void main(String[] args) {
+        int cnt = 0;
+        while (cnt < 11) {
+            System.out.println(cnt++);
+        }
+        System.out.println("cnt = " + cnt);
+    }
+}
---
+++ src/main/java/com/example/javaproject2/week3/day2/WhileCntHori.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week3.day2;
+
+public class WhileCntHori {
+    public static void main(String[] args) {
+        int cnt = 0;
+        while (cnt < 11) {
+            System.out.printf("%d ", cnt++);
+        }
+    }
+}
