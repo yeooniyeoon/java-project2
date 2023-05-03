4008188278b147c1299cb719fad3c09091bdb19f
ycyeon <ickite428@gmail.com>
2023-05-03 18:23:13 +0900

    fix: Codeup1620 자릿수 합 구하기 메소드로 추가

--- src/main/java/com/example/javaproject2/codeup/Codeup1620.java
+++ src/main/java/com/example/javaproject2/codeup/Codeup1620.java
@@ -3,21 +3,21 @@ package com.example.javaproject2.codeup;
 import java.util.Scanner;
 
 public class Codeup1620 {
+    public static int sumOfDigit(int num) {
+        int sum = 0;
+        while (num > 9) {
+            sum += num % 10;
+            num /= 10;
+        }
+        sum += num;
+        return sum;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
-        int sum = 0;
-        while (input > 9) {
-            sum += input % 10;
-            input /= 10;
-        }
-        sum += input;
-        while (sum > 9) {
-            input = sum % 10;
-            sum /= 10;
-            input += sum;
-            sum = input;
-        }
-        System.out.println(sum);
+        input = sumOfDigit(input);
+        while (input > 9)
+            input = sumOfDigit(input);
+        System.out.println(input);
     }
 }
