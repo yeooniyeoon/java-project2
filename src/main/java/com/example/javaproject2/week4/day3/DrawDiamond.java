e66225edb7165a9c78e3ccd654d0656e6fed50e0
ycyeon <ickite428@gmail.com>
2023-05-10 09:40:37 +0900

    feat: DrawDiamond 추가

---
+++ src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
@@ -0,0 +1,19 @@
+package com.example.javaproject2.week4.day3;
+
+public class DrawDiamond {
+    public static String getRepeatSymbol(String symbol, int n) {
+        return symbol.repeat(n);
+    }
+    public static void main(String[] args) {
+        int h = 7;
+        int pivot = h / 2;
+        for (int i = 0; i < h; i++) {
+            if (i <= pivot) {
+                System.out.printf("%s%s\n", getRepeatSymbol("0", -2 + h - i - 2), getRepeatSymbol("*", 2 * i + 1));
+            } else {
+                System.out.printf("%s%s\n", getRepeatSymbol("0", i - pivot), getRepeatSymbol("*", 2 * (h - i) - 1));
+            }
+
+        }
+    }
+}
