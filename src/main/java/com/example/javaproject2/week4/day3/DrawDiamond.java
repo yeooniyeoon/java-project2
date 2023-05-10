98673b7f61f23bab6d68f1d70e7f59312f9b1a69
ycyeon <ickite428@gmail.com>
2023-05-10 10:11:31 +0900

    fix: DrawDiamond repeat사용하지 않고 출력하도록 수정

--- src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
+++ src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
@@ -2,18 +2,18 @@ package com.example.javaproject2.week4.day3;
 
 public class DrawDiamond {
     public static String getRepeatSymbol(String symbol, int n) {
-        return symbol.repeat(n);
+        String str = "";
+        for (int i = 0; i < n; i++) str += symbol;
+        return str;
     }
     public static void main(String[] args) {
         int h = 7;
         int pivot = h / 2;
         for (int i = 0; i < h; i++) {
-            if (i <= pivot) {
+            if (i <= pivot)
                 System.out.printf("%s%s\n", getRepeatSymbol("0", -2 + h - i - 2), getRepeatSymbol("*", 2 * i + 1));
-            } else {
+            else
                 System.out.printf("%s%s\n", getRepeatSymbol("0", i - pivot), getRepeatSymbol("*", 2 * (h - i) - 1));
-            }
-
         }
     }
 }
