7ba3654882053cd4f65968818e77b6535c8be3e6
ycyeon <ickite428@gmail.com>
2023-05-10 10:16:30 +0900

    fix: DrawDiamond 출력 부분 makeALine()으로 분리

--- src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
+++ src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
@@ -6,14 +6,21 @@ public class DrawDiamond {
         for (int i = 0; i < n; i++) str += symbol;
         return str;
     }
+
+    public static String makeALine(int h, int i) {
+        int pivot = h / 2;
+        if (i <= pivot)
+            return String.format("%s%s\n", getRepeatSymbol("0", -2 + h - i - 2), getRepeatSymbol("*", 2 * i + 1));
+        else
+            return String.format("%s%s\n", getRepeatSymbol("0", i - pivot), getRepeatSymbol("*", 2 * (h - i) - 1));
+    }
+
     public static void main(String[] args) {
         int h = 7;
         int pivot = h / 2;
         for (int i = 0; i < h; i++) {
-            if (i <= pivot)
-                System.out.printf("%s%s\n", getRepeatSymbol("0", -2 + h - i - 2), getRepeatSymbol("*", 2 * i + 1));
-            else
-                System.out.printf("%s%s\n", getRepeatSymbol("0", i - pivot), getRepeatSymbol("*", 2 * (h - i) - 1));
+            if (i <= pivot) makeALine(h, i);
+            else makeALine(h, i);
         }
     }
 }
