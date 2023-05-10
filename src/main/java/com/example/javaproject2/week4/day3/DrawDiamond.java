230d2bafdc9cee9cb6a89d5069d3090b67965f4c
ycyeon <ickite428@gmail.com>
2023-05-10 11:01:37 +0900

    fix: DrawDiamond makeALine에서 출력하는 부분 printShape()로 분리

--- src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
+++ src/main/java/com/example/javaproject2/week4/day3/DrawDiamond.java
@@ -1,13 +1,23 @@
 package com.example.javaproject2.week4.day3;
 
-public class DrawDiamond {
+import com.example.javaproject2.week4.day2.ShapeDrawer;
+import javafx.scene.shape.Shape;
+
+public class DrawDiamond extends ShapeDrawer {
     public static String getRepeatSymbol(String symbol, int n) {
         String str = "";
         for (int i = 0; i < n; i++) str += symbol;
         return str;
     }
 
-    public static String makeALine(int h, int i) {
+    public void printShape(int h) {
+        for (int i = 0; i < h; i++) {
+            System.out.printf(makeALine(h, i));
+        }
+    }
+
+    @Override
+    public String makeALine(int h, int i) {
         int pivot = h / 2;
         if (i <= pivot)
             return String.format("%s%s\n", getRepeatSymbol("0", -2 + h - i - 2), getRepeatSymbol("*", 2 * i + 1));
@@ -16,11 +26,9 @@ public class DrawDiamond {
     }
 
     public static void main(String[] args) {
-        int h = 7;
-        int pivot = h / 2;
-        for (int i = 0; i < h; i++) {
-            if (i <= pivot) makeALine(h, i);
-            else makeALine(h, i);
-        }
+        DrawDiamond drawDiamond = new DrawDiamond();
+        int h = 5;
+        drawDiamond.printShape(h);
+
     }
 }
